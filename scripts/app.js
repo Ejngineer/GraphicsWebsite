bb89683252f1e36a9306dbb9d082d8fe9065fbc1
Emanuel Redondo <eredo001@ucr.edu>
2022-04-21 07:36:12 -0400

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.getElementById('github').onclick = function()
+{
+    window.open('https://github.com/Ejngineer?tab=repositories','_self');
+}
+
+document.getElementById("projects").onclick = function()
+{
+    setTimeout(window.open("https://youtube.com",'_self'), 1000);
+}
